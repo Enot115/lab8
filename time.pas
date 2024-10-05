982d29df2a5985165ebf783d5ceb935a698a767f
unknown <user@mil8a-202a-05.mmcs.sfedu.ru>
2024-10-05 06:38:18 +0300

    add time.pas

---
+++ time.pas
@@ -0,0 +1,7 @@
+﻿begin
+  var x := ReadInteger('Введите год:');
+  if (x mod 4 = 0) and (x mod 100 <> 0) and (x mod 400 <> 0) then
+    Print('Год високосный')
+  else
+    print('Год не високосный');
+end.
